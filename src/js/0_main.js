6025c38a99e4069852a7fd203d644218f9f0e81a
Silvia <silviatc1993@gmail.com>
2023-09-12 14:08:03 +0200

    javascript first structure

---
+++ src/js/0_main.js
@@ -0,0 +1,57 @@
+'use strict';
+
+//Query selectors
+const design = document.querySelector('.js-design');
+const fill = document.querySelector('.js-fill');
+const share = document.querySelector('.js-share');
+//Datos generales-variables globales
+
+//Funciones
+
+//funciones Open/close Design form
+function openDesign() {
+  design.classList.remove('collapsed');
+}
+function closeDesign() {
+  design.classList.add('collapsed');
+}
+//funciones Open/close Fill form
+function openFill() {
+  fill.classList.remove('collapsed');
+}
+function closeFill() {
+  fill.classList.add('collapsed');
+}
+//funciones Open/close Share form
+function openShare() {
+  share.classList.remove('collapsed');
+}
+function closeShare() {
+  share.classList.add('collapsed');
+}
+
+//funciones del listener para cada formulario
+
+//Función handle listener elemento Form Design
+function handleClickDesign() {
+  openDesign();
+  closeFill();
+  closeShare();
+}
+//Función handle listener elemento Form Fill
+function handleClickFill() {
+  closeDesign();
+  openFill();
+  closeShare();
+}
+//Función handle listener elemento Form Share
+function handleClickShare() {
+  closeDesign();
+  closeFill();
+  openShare();
+}
+
+//Eventos
+design.addEventListener('click', handleClickDesign);
+fill.addEventListener('click', handleClickFill);
+share.addEventListener('click', handleClickShare);
