dd6d32e790067d3d1cd10e85bb9560237ce3286a
Irene García <irenegwodak@gmail.com>
2023-09-15 12:49:49 +0200

    add js-arrow-rotate & fix code to adjust mediaquery

--- src/js/0_main.js
+++ src/js/0_main.js
@@ -4,9 +4,12 @@
 const design = document.querySelector('.js-design');
 const fill = document.querySelector('.js-fill');
 const share = document.querySelector('.js-share');
-const fillFormContent = document.querySelector('.js-fillFormContent');
 const designFormContent = document.querySelector('.js-designFormContent');
+const fillFormContent = document.querySelector('.js-fillFormContent');
 const shareFormContent = document.querySelector('.js-shareFormContent');
+const designArrow = document.querySelector('.js-design-arrow');
+const fillArrow = document.querySelector('.js-fill-arrow');
+const shareArrow = document.querySelector('.js-share-arrow');
 
 //Datos generales-variables globales
 
@@ -15,23 +18,29 @@ const shareFormContent = document.querySelector('.js-shareFormContent');
 //funciones Open/close Design form
 function openDesign() {
   designFormContent.classList.remove('collapsed');
+  designArrow.classList.remove('rotate__arrow');
 }
 function closeDesign() {
   designFormContent.classList.add('collapsed');
+  designArrow.classList.add('rotate__arrow');
 }
 //funciones Open/close Fill form
 function openFill() {
   fillFormContent.classList.remove('collapsed');
+  fillArrow.classList.remove('rotate__arrow');
 }
 function closeFill() {
   fillFormContent.classList.add('collapsed');
+  fillArrow.classList.add('rotate__arrow');
 }
 //funciones Open/close Share form
 function openShare() {
   shareFormContent.classList.remove('collapsed');
+  shareArrow.classList.remove('rotate__arrow');
 }
 function closeShare() {
   shareFormContent.classList.add('collapsed');
+  shareArrow.classList.add('rotate__arrow');
 }
 
 //funciones del listener para cada formulario
