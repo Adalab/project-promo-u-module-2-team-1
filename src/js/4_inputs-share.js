7466b8fbd46d54d562119a899a8bfe8f44edea52
cristinanico <135840337+cristinanico@users.noreply.github.com>
2023-09-26 13:46:24 +0200

    working on share

--- src/js/4_inputs-share.js
+++ src/js/4_inputs-share.js
@@ -4,6 +4,11 @@
 const btnShare = document.querySelector('.js_btn_card');
 const msgShareError = document.querySelector('.js_share_error');
 const shareSection = document.querySelector('.js-share__result');
+
+const urlTwitter = document.querySelector('.js__twitter');
+const urlCard = document.querySelector('.js__cardUrl');
+const msgBox = document.querySelector('.js__msgBox');
+
 function handleBtnShare(event) {
   event.preventDefault();
   const patron = /^[0-9]+$/;
@@ -26,7 +31,22 @@ function handleBtnShare(event) {
   } else {
     msgShareError.innerHTML = 'Faltan campos por rellenar';
   }
-  
+
+  fetch('https://dev.adalab.es/api/card/', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((responseJSON) => {
+      if (responseJSON.success === false) {
+        msgBox.innerHTML = 'Faltan campos por rellenar';
+      } else {
+        shareSection.classList.remove('collapsed');
+        urlCard.href = responseJSON.cardURL;
+        urlTwitter.innerHTML = responseJSON.cardURL;
+      }
+    });
 }
 
 btnShare.addEventListener('click', handleBtnShare);
