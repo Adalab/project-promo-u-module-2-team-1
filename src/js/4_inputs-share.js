475aaf45a279f944d199dafcda5ca2c0f6b78e7d
Irene García <irenegwodak@gmail.com>
2023-09-28 00:25:57 +0200

    fix validation inputs messages error

--- src/js/4_inputs-share.js
+++ src/js/4_inputs-share.js
@@ -13,42 +13,62 @@ function handleBtnShare(event) {
   event.preventDefault();
   const patron = /^[0-9]+$/;
 
-  fetch('https://dev.adalab.es/api/card/', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(data),
-  })
-    .then((response) => response.json())
-    .then((responseJSON) => {
-      if (responseJSON.success === false) {
-        msgShareError.innerHTML = 'Faltan campos por rellenar';
-      } else {
-        shareSection.classList.remove('collapsed');
-        urlCard.href = responseJSON.cardURL;
-        urlCard.innerHTML = responseJSON.cardURL;
-        urlTwitter.href = `https://twitter.com/intent/tweet?url=${responseJSON.cardURL}&text=Dicen+que+las+tarjetas+de+presentaci%c3%b3n+han+muerto%2c+pero...+%c2%a1qu%c3%a9+va!+Aqu%c3%ad+os+dejo+la+m%c3%ada+%3b)+Si+quieres+una+tarjeta+igual+de+guay+haz+click+aqu%c3%ad+https%3a%2f%2ftinyurl.com%2fadapickleteam%0d%0a%0d%0a`;
-      }
-      localStorage.setItem('definitiveCard', JSON.stringify(responseJSON));
-    });
-
+  // Validar si todos los campos están rellenados:
   if (
     inputName.value === '' ||
     inputJob.value === '' ||
     inputPhone.value === '' ||
     inputEmail.value === '' ||
     inputLinkedin.value === '' ||
-    inputGithub.value === '' ||
-    inputPhoto.value === ''
+    inputGithub.value === ''
   ) {
+    //Si faltan campos por rellenar
     msgShareError.innerHTML = 'Faltan campos por rellenar';
-  } else if (inputEmail.value.includes(`. @`)) {
-    msgShareError.innerHTML = '';
-    shareSection.classList.remove('collapsed');
-  } else if (patron.test(inputPhone.value)) {
-    msgShareError.innerHTML = '';
-    shareSection.classList.remove('collapsed');
   } else {
-    msgShareError.innerHTML = 'Faltan campos por rellenar';
+    //Si todos los campos están rellenados, comprobar si hay foto:
+    if (inputPhoto.value === '') {
+      msgShareError.innerHTML = 'Añade una imagen para continuar';
+    } else {
+      // Si hay foto, validar la dirección de correo electrónico:
+      if (inputEmail.value.includes('@') && inputEmail.value.includes('.')) {
+        // Correo electrónico es ok
+        // Validar el número de teléfono usando la expresión regular:
+        if (patron.test(inputPhone.value)) {
+          // El número de teléfono es válido
+          msgShareError.innerHTML = '';
+          // Todo ok, realizamos fetch
+          fetch('https://dev.adalab.es/api/card/', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data),
+          })
+            .then((response) => response.json())
+            .then((responseJSON) => {
+              if (responseJSON.success === false) {
+                msgShareError.innerHTML = responseJSON.error;
+              } else {
+                shareSection.classList.remove('collapsed');
+                urlCard.href = responseJSON.cardURL;
+                urlCard.innerHTML = responseJSON.cardURL;
+                urlTwitter.href = `https://twitter.com/intent/tweet?url=${responseJSON.cardURL}&text=Dicen+que+las+tarjetas+de+presentaci%c3%b3n+han+muerto%2c+pero...+%c2%a1qu%c3%a9+va!+Aqu%c3%ad+os+dejo+la+m%c3%ada+%3b)+Si+quieres+una+tarjeta+igual+de+guay+haz+click+aqu%c3%ad+https%3a%2f%2ftinyurl.com%2fadapickleteam%0d%0a%0d%0a`;
+              }
+              localStorage.setItem(
+                'definitiveCard',
+                JSON.stringify(responseJSON)
+              );
+            });
+        } else {
+          // El número de teléfono no es válido
+          msgShareError.innerHTML = 'Por favor, ingresa una teléfono válido.';
+          shareSection.classList.add('collapsed');
+        }
+      } else {
+        // La dirección de correo electrónico no es válida
+        msgShareError.innerHTML =
+          'Por favor, ingresa una dirección de correo electrónico válida.';
+        shareSection.classList.add('collapsed');
+      }
+    }
   }
 }
 const dataLS = JSON.parse(localStorage.getItem('definitiveCard'));
