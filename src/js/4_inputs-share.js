62ac3833538ac54f5869bb0654aaaf5a54f17542
Irene García <irenegwodak@gmail.com>
2023-09-27 18:16:42 +0200

    delete second warning + change twitter message

--- src/js/4_inputs-share.js
+++ src/js/4_inputs-share.js
@@ -8,7 +8,6 @@ const shareSection = document.querySelector('.js-share__result');
 
 const urlTwitter = document.querySelector('.js__twitter');
 const urlCard = document.querySelector('.js__cardUrl');
-const msgBox = document.querySelector('.js__msgBox');
 
 function handleBtnShare(event) {
   event.preventDefault();
@@ -41,12 +40,13 @@ function handleBtnShare(event) {
     .then((response) => response.json())
     .then((responseJSON) => {
       if (responseJSON.success === false) {
-        msgBox.innerHTML = 'Faltan campos por rellenar';
+        console.log(responseJSON);
+        msgShareError.innerHTML = 'Servidor caído, inténtalo más tarde';
       } else {
         shareSection.classList.remove('collapsed');
         urlCard.href = responseJSON.cardURL;
         urlCard.innerHTML = responseJSON.cardURL;
-        urlTwitter.href = `https://twitter.com/intent/tweet?url=${responseJSON.cardURL}&text=Ya%20estoy%20suscrita%20al%20Adalab%20Tech%20Summit%21%21%21&hashtags=Adalab`;
+        urlTwitter.href = `https://twitter.com/intent/tweet?url=${responseJSON.cardURL}&text=Dicen+que+las+tarjetas+de+presentaci%c3%b3n+han+muerto%2c+pero...+%c2%a1qu%c3%a9+va!+Aqu%c3%ad+os+dejo+la+m%c3%ada+%3b)+Si+quieres+una+tarjeta+igual+de+guay+haz+click+aqu%c3%ad+https%3a%2f%2ftinyurl.com%2fadapickleteam%0d%0a%0d%0a`;
       }
     });
   localStorage.setItem('definitiveCard', JSON.stringify(responseJSON));
